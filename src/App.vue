9068c9525f83fd825f2d745d8f17a64dfc0f93e4
mukhsamr <mukhsamr@gmail.com>
2024-06-01 14:13:45 +0700

    feat: add content, refactor style

--- src/App.vue
+++ src/App.vue
@@ -44,10 +44,10 @@ onMounted(() => {
 </script>
 
 <template>
-  <nav class="fixed w-full z-10 bg-white shadow flex md:justify-center transition-transform"
+  <nav class="fixed w-full z-10 bg-white shadow flex sm:justify-center transition-transform"
     :class="{ '-translate-y-36': hideNav }">
 
-    <ul class="flex justify-around py-6 w-screen md:max-w-lg text-sm overflow-hidden">
+    <ul class="flex justify-around py-6 w-screen sm:max-w-lg text-sm overflow-hidden">
       <li>
         <a href="#home" class="block cursor-pointer"
           :class="activeNav == 'home' ? 'text-green-700 font-bold' : 'hover:text-green-700'">Home</a>
@@ -79,25 +79,25 @@ onMounted(() => {
         class="h-full flex items-center border-b-4 border-slate-700 bg-gradient-to-tr from-green-200/30 to-white/30 backdrop-blur-sm">
         <div class="">
           <div class="text-slate-800">
-            <div class="px-12 md:px-28">
+            <div class="px-12 sm:px-28">
               <div class="relative text-center">
 
                 <div class="absolute -top-5 -left-5 fill-green-600">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-8 md:h-12" viewBox="0 0 448 512">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-8 sm:h-12" viewBox="0 sm:0 448 512">
                     <path
                       d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" />
                   </svg>
                 </div>
 
-                <div class="text-xl md:text-4xl">
+                <div class="text-xl sm:text-4xl">
                   Mencetak Generasi Islami yang <strong>Cerdas</strong>, <strong>Tangguh</strong> dan <strong>Berakhlak
                     Mulia</strong>
                 </div>
 
                 <div class="absolute -bottom-5 -right-5 fill-green-600">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-8 md:h-12" viewBox="0 0 448 512">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-8 sm:h-12" viewBox="0 sm:0 448 512">
                     <path
-                      d="M448 296c0 66.3-53.7 120-120 120h-8c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H320c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72zm-256 0c0 66.3-53.7 120-120 120H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72z" />
+                      dsm:="M448 296c0 66.3-53.7 120-120 120h-8c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H320c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72zm-256 0c0 66.3-53.7 120-120 120H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72z" />
                   </svg>
                 </div>
 
@@ -106,13 +106,13 @@ onMounted(() => {
 
             <hr class="my-10 w-0">
 
-            <div class="text-center px-4 md:px-16">
-              <div class="text-xl md:text-3xl font-[NotoNaskh]">
+            <div class="text-center px-4 sm:px-16">
+              <div class="text-xl sm:text-3xl font-[NotoNaskh]">
                 اِنَّ هٰذَا الۡقُرۡاٰنَ يَهۡدِىۡ لِلَّتِىۡ هِىَ اَقۡوَمُ وَ يُبَشِّرُ الۡمُؤۡمِنِيۡنَ الَّذِيۡنَ
                 يَعۡمَلُوۡنَ
                 الصّٰلِحٰتِ اَنَّ لَهُمۡ اَجۡرًا كَبِيۡرًا
               </div>
-              <div class="mt-6 text-sm md:text-xl">
+              <div class="mt-6 text-sm sm:text-xl">
                 "Sungguh, Al Qur'an ini memberi petunjuk ke (jalan) yang paling lurus dan memberi kabar gembira kepada
                 orang
                 mukmin
@@ -129,60 +129,52 @@ onMounted(() => {
 
     <!-- Program -->
     <section id="program" class="min-h-screen">
-      <div class="py-20 px-4 md:px-24 text-center">
-        <div class="text-3xl font-semibold" data-aos="fade-up">Program Kami</div>
+      <div class="py-20 px-4 sm:px-24">
+        <div class="text-xl sm:text-3xl font-semibold text-center" data-aos="fade-up">Program Kami</div>
 
-        <div class="grid grid-cols-1 md:grid-cols-3 justify-center mt-10 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-3 justify-center content-stretch mt-10 gap-4">
 
           <div data-aos="fade-up">
-            <div class="transition-transform hover:scale-105">
-              <div class="flex space-x-3 border-4 border-black p-4 items-center bg-white rounded-t-xl">
-                <img draggable="false" src="/icon/rocket.webp" alt="rocket" class="w-20 h-20">
-                <div class="text-xl font-semibold">Taman Kanak-Kanak Qur'an</div>
+            <div class="transition-transform hover:scale-105 flex flex-col h-full">
+              <div class="flex space-x-4 border-4 border-black p-4 items-center bg-white rounded-t-xl">
+                <img draggable="false" src="/icon/rocket.webp" alt="rocket" class="w-14 h-14 sm:w-20 sm:h-20">
+                <div class="text-lg sm:text-xl font-semibold">Taman Kanak-Kanak Qur'an</div>
               </div>
-
-
-              <div class="border-x-4 border-b-4 border-black px-6 py-4 bg-blue-200 rounded-b-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ipsam, sed inventore accusantium
-                quas
-                ipsa
-                sint deserunt eveniet eum neque.
+              <div class="grow flex items-center border-x-4 border-b-4 border-black px-6 py-4 bg-blue-200 rounded-b-xl">
+                Program ini menggabungkan pembelajaran Islami dengan metode interaktif, memperkenalkan huruf Hijaiyah,
+                doa-doa harian, dan kisah nabi melalui permainan dan kegiatan kreatif.
               </div>
-
             </div>
           </div>
 
           <div data-aos="fade-up" data-aos-delay="100">
-            <div class="transition-transform hover:scale-105">
-              <div class="flex space-x-3 border-4 border-black p-4 items-center bg-white rounded-t-xl">
-                <img draggable="false" src="/icon/quran.webp" alt="quran" class="w-20 h-20">
-                <div class="text-xl font-semibold">Taman Pendidikan Qur'an</div>
+            <div class="transition-transform hover:scale-105 flex flex-col h-full">
+              <div class="flex space-x-4 border-4 border-black p-4 items-center bg-white rounded-t-xl">
+                <img draggable="false" src="/icon/quran.webp" alt="quran" class="w-14 h-14 sm:w-20 sm:h-20">
+                <div class="text-lg sm:text-xl font-semibold">Taman Pendidikan Qur'an</div>
               </div>
 
-
-              <div class="border-x-4 border-b-4 border-black px-6 py-4 bg-green-200 rounded-b-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ipsam, sed inventore accusantium
-                quas
-                ipsa
-                sint deserunt eveniet eum neque.
+              <div
+                class="grow flex items-center border-x-4 border-b-4 border-black px-6 py-4 bg-green-200 rounded-b-xl">
+                Program Ini dirancang untuk mempelajari dasar-dasar pengetahuan Al Quran dari mulai mengenal huruf
+                hijaiah, membaca, menghafal sampai belajar hukum-hukum tajwid.
+                Anak-anak juga diajak untuk mengenal dan menghafal doa-doa harian.
               </div>
 
             </div>
           </div>
 
           <div data-aos="fade-up" data-aos-delay="200">
-            <div class="transition-transform hover:scale-105">
-              <div class="flex space-x-3 border-4 border-black p-4 items-center bg-white rounded-t-xl">
-                <img draggable="false" src="/icon/toga.webp" alt="toga" class="w-20 h-20">
-                <div class="text-xl font-semibold">Lembaga Pendidikan Qur'an</div>
+            <div class="transition-transform hover:scale-105 flex flex-col h-full">
+              <div class="flex space-x-4 border-4 border-black p-4 items-center bg-white rounded-t-xl">
+                <img draggable="false" src="/icon/toga.webp" alt="toga" class="w-14 h-14 sm:w-20 sm:h-20">
+                <div class="text-lg sm:text-xl font-semibold">Lembaga Pendidikan Qur'an</div>
               </div>
 
-
-              <div class="border-x-4 border-b-4 border-black px-6 py-4 bg-lime-200 rounded-b-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ipsam, sed inventore accusantium
-                quas
-                ipsa
-                sint deserunt eveniet eum neque.
+              <div class="grow flex items-center border-x-4 border-b-4 border-black px-6 py-4 bg-lime-200 rounded-b-xl">
+                Program ini dirancang untuk memperkuat pengetahuan Al Quran yang telah mereka peroleh,
+                dengan fokus pada peningkatan ke-fashihan bacaan, memperdalam hukum-hukum tajwid. Anak-anak juga akan
+                diajarkan kitab-kitab Aqidah, Nahwu Sharaf dan sejenisnya.
               </div>
 
             </div>
@@ -196,26 +188,26 @@ onMounted(() => {
 
     <!-- Galery -->
     <section id="galery" class="min-h-screen">
-      <div class="bg-[url('/bg/bg.webp')] px-2 py-16 md:px-28 md:py-20 bg-slate-700">
-        <div class="text-3xl font-semibold text-center text-white" data-aos="fade-up">Galery</div>
+      <div class="bg-[url('/bg/bg.webp')] px-2 py-16 sm:px-28 sm:py-20 bg-slate-700">
+        <div class="text-xl sm:text-3xl font-semibold text-center text-white" data-aos="fade-up">Galery</div>
 
         <div class="mx-auto py-2 mt-8">
           <div class="flex flex-wrap">
 
-            <div class="flex md:w-1/2 flex-wrap">
-              <div class="w-1/2 p-1 md:p-2 transition-transform hover:scale-105">
+            <div class="flex sm:w-1/2 flex-wrap">
+              <div class="w-1/2 p-1 sm:p-2 transition-transform hover:scale-105">
 
                 <img draggable="false" alt="gallery-img-1"
                   class="h-full w-full rounded-lg object-cover object-center border-4 border-white" data-aos="zoom-in"
                   src="/galery/galery-1.webp" />
               </div>
-              <div class="w-1/2 p-1 md:p-2 transition-transform hover:scale-105">
+              <div class="w-1/2 p-1 sm:p-2 transition-transform hover:scale-105">
 
                 <img draggable="false" alt="gallery-img-2"
                   class="h-full w-full rounded-lg object-cover object-center border-4 border-white" data-aos="zoom-in"
                   src="/galery/galery-2.webp" />
               </div>
-              <div class="w-full p-1 md:p-2 transition-transform hover:scale-105">
+              <div class="w-full p-1 sm:p-2 transition-transform hover:scale-105">
 
                 <img draggable="false" alt="gallery-img-3"
                   class="h-full w-full rounded-lg object-cover object-center border-4 border-white" data-aos="zoom-in"
@@ -223,18 +215,18 @@ onMounted(() => {
               </div>
             </div>
 
-            <div class="flex md:w-1/2 flex-wrap">
-              <div class="w-full p-1 md:p-2 transition-transform hover:scale-105">
+            <div class="flex sm:w-1/2 flex-wrap">
+              <div class="w-full p-1 sm:p-2 transition-transform hover:scale-105">
                 <img draggable="false" alt="gallery-img-4"
                   class="h-full w-full rounded-lg object-cover object-center border-4 border-white" data-aos="zoom-in"
                   src="/galery/galery-6.webp" />
               </div>
-              <div class="w-1/2 p-1 md:p-2 transition-transform hover:scale-105">
+              <div class="w-1/2 p-1 sm:p-2 transition-transform hover:scale-105">
                 <img draggable="false" alt="gallery-img-5"
                   class="h-full w-full rounded-lg object-cover object-center border-4 border-white" data-aos="zoom-in"
                   src="/galery/galery-3.webp" />
               </div>
-              <div class="w-1/2 p-1 md:p-2 transition-transform hover:scale-105">
+              <div class="w-1/2 p-1 sm:p-2 transition-transform hover:scale-105">
                 <img draggable="false" alt="gallery-img-6"
                   class="h-full w-full rounded-lg object-cover object-center border-4 border-white" data-aos="zoom-in"
                   src="/galery/galery-4.webp" />
@@ -244,7 +236,7 @@ onMounted(() => {
         </div>
 
         <a href="https://photos.app.goo.gl/rLFqLhLakGhZHoav7" target="_blank"
-          class="text-white border-2 rounded border-white px-2 py-1 hover:bg-white hover:text-slate-700 transition-all">
+          class="inline-block rounded bg-white px-4 py-2 mt-2 ml-2 hover:bg-slate-900 hover:text-white transition-all">
           Lihat lebih banyak
         </a>
 
@@ -254,17 +246,17 @@ onMounted(() => {
 
     <!-- History -->
     <section id="history" class="min-h-screen">
-      <div class="pt-20 md:px-28">
-        <div class="text-3xl px-4 md:p-0 md:text-4xl text-center">
-          <span class="font-semibold md:p-4" data-aos="fade-up">History</span>
+      <div class="pt-20 sm:px-28">
+        <div class="text-xl sm:text-3xl px-4 sm:p-0 sm:text-4xl text-center">
+          <span class="font-semibold sm:p-4" data-aos="fade-up">History</span>
         </div>
 
-        <div class="mt-6 p-4 md:p-10 shadow border-4 border-slate-700 relative bg-[url('/bg/paper.webp')] bg-cover"
+        <div class="mt-6 p-4 sm:p-10 shadow border-4 border-slate-700 relative bg-[url('/bg/paper.webp')] bg-cover"
           data-aos="fade-up">
 
-          <div class="mb-6 md:w-96 md:float-right md:ml-6">
+          <div class="mb-6 sm:w-96 sm:float-right sm:ml-6">
             <img draggable="false" src="/bg/halaman.webp" alt="halaman"
-              class="rounded-xl w-[21rem] h-64 md:w-96 md:h-72">
+              class="rounded-xl w-[21rem] h-64 sm:w-96 sm:h-72">
           </div>
           <div class="leading-relaxed">
             <p>
@@ -304,10 +296,10 @@ onMounted(() => {
 
         <div class="my-20 space-y-10">
 
-          <div class="grid grid-cols-1 md:grid-cols-4 gap-8" data-aos="fade-up">
+          <div class="grid grid-cols-2 sm:grid-cols-4 sm:gap-8 gap-4" data-aos="fade-up">
             <div>
               <img draggable="false" src="/guru/abi.webp" alt="abi"
-                class="w-44 h-44 mx-auto rounded-full object-contain object-center border-4 border-black transition-transform hover:-translate-y-3 hover:shadow-xl">
+                class="w-40 h-40 sm:w-44 sm:h-44 mx-auto rounded-full object-contain object-center border-4 border-black transition-transform hover:-translate-y-3 hover:shadow-xl">
 
               <div class="text-center mt-3">
                 <div class="font-semibold">- Pembina Yayasan -</div>
@@ -318,7 +310,7 @@ onMounted(() => {
 
             <div>
               <img draggable="false" src="/guru/umi.webp" alt="umi"
-                class="w-44 h-44 mx-auto rounded-full object-contain object-center border-4 border-black transition-transform hover:-translate-y-3 hover:shadow-xl">
+                class="w-40 h-40 sm:w-44 sm:h-44 mx-auto rounded-full object-contain object-center border-4 border-black transition-transform hover:-translate-y-3 hover:shadow-xl">
 
               <div class="text-center mt-3">
                 <div class="font-semibold">- Pembina Yayasan -</div>
@@ -328,7 +320,7 @@ onMounted(() => {
 
             <div>
               <img draggable="false" src="/guru/user.webp" alt="user"
-                class="w-44 h-44 mx-auto rounded-full object-contain object-center border-4 border-black">
+                class="w-40 h-40 sm:w-44 sm:h-44 mx-auto rounded-full object-contain object-center border-4 border-black">
 
               <div class="text-center mt-3">
                 <div class="font-semibold">- Ketua Yayasan -</div>
@@ -338,7 +330,7 @@ onMounted(() => {
 
             <div>
               <img draggable="false" src="/guru/user.webp" alt="user"
-                class="w-44 h-44 mx-auto rounded-full object-contain object-center border-4 border-black">
+                class="w-40 h-40 sm:w-44 sm:h-44 mx-auto rounded-full object-contain object-center border-4 border-black">
 
               <div class="text-center mt-3">
                 <div class="font-semibold">- ??? -</div>
@@ -355,8 +347,8 @@ onMounted(() => {
     <!-- Contact -->
     <section id="kontak">
       <footer class="border-t-4 border-black bg-lime-200">
-        <div class="py-10 px-4 md:px-28 bg-[url('/bg/bg.webp')]">
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
+        <div class="py-10 px-4 sm:px-28 bg-[url('/bg/bg.webp')]">
+          <div class="grid grid-cols-1 sm:grid-cols-2 gap-12">
             <div data-aos="fade-up">
               <div class="text-2xl font-semibold"># Contact</div>
 
@@ -409,7 +401,7 @@ onMounted(() => {
           </div>
         </div>
 
-        <div class="font-black px-4 md:px-28 py-4 text-right">
+        <div class="font-black px-4 sm:px-28 py-4 text-right">
           Copyright © {{ new Date().getFullYear() }} nurulquranmulia.sch.id
         </div>
       </footer>
